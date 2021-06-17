c4075ac13cb354077ab657f0fa4f3d1a0e8691e0
Tony7-the-Human <75579433+Tony7-the-Human@users.noreply.github.com>
2021-06-17 22:03:48 +0300

    Update app.js

--- app.js
+++ app.js
@@ -15,8 +15,17 @@ upBtn.addEventListener('click', () => {
 })
 
 
+
 downBtn.addEventListener('click', () => {
     changeSlide('down')
+})
+
+document.addEventListener('keydown', event => {
+	if (event.key === 'ArrowUp') {
+		changeSlide('up')
+	}else if (event.key === 'ArrowDown'){
+		changeSlide('down')
+	}
 })
 
 function changeSlide (direction) {
